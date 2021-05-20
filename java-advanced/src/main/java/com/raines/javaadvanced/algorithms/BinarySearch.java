f7c02f76b41fb6a01c0065b728d40e7a9adaa8f1
liuxiaolong <17192192678@163.com>
2021-05-20 20:21:03 +0800

    updat::

---
+++ java-advanced/src/main/java/com/raines/javaadvanced/algorithms/BinarySearch.java
@@ -0,0 +1,48 @@
+package com.raines.javaadvanced.algorithms;
+
+/**
+ * 二分查找算法
+ * 二分查找又叫作折半查找，二分查找算法要求要查找的序列是有序的，每次查找都取中间位置的值与待查关键字进行比较，如果中间位置的值比待查关键字大，则在序列的左半部分继续执行该查找过程，如果中间位置的值比待查关键字小，则在序列的右半部分继续执行该查找过程，直到查找到关键字为止，否则在序列中没有待查关键字。
+ */
+public class BinarySearch {
+
+    /**
+     * 二分查找
+     *
+     * @param array 源数组
+     * @param a     目标值
+     * @return 目标值在数组中索引位置，未找到返回-1
+     */
+    public static int binarySearch(int[] array, int a) {
+        // 最小数据索引
+        int low = 0;
+        //最大数据索引
+        int high = array.length - 1;
+        //中间数据索引
+        int mid;
+        //通过while循环在数组中查找传入的数据，在该数据大于中间位置的数据时向右查找，即最大索引位置不变，将最小索引设置为上次循环的中间索引+1；在该数据小于中间位置的数据时向左查找，即最小索引位置不变，然后将最大索引设置为上次循环的中间索引并-1。重复以上过程，直到中间索引位置的数据等于要查找的数据，则将该数据对应的索引返回。
+        while (low <= high) {
+            //中间位置
+            mid = (low + high) / 2;
+            if (array[mid] == a) {
+                return mid;
+            } else if (a > array[mid]) {
+                //从右查找
+                low = mid + 1;
+            } else {
+                //向左查找
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 111; i < 211; i++) {
+            array[i - 111] = i;
+        }
+        System.out.println(binarySearch(array, 188));
+    }
+
+}
---
+++ java-advanced/src/main/java/com/raines/javaadvanced/algorithms/bubbleSort.java
@@ -0,0 +1,41 @@
+package com.raines.javaadvanced.algorithms;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 冒泡排序算法
+ * 它在重复访问要排序的元素列时，会依此比较相邻的两个元素，如果左边的元素大于右边的元素，就将二者交换位置，如此重复，直到没有相邻的元素需要交换位置，这时该列表的元素排序完成。
+ */
+public class bubbleSort {
+
+    /**
+     * 升序冒泡排序算法
+     * @param arr 源数组
+     * @return 升序排序数组
+     */
+    public static int[] bubbleSort(int[] arr){
+        //外层循环控制排序趟数
+        for (int i = 0; i < arr.length - 1; i++) {
+            //内层循环控制每一趟排序多少次
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                //比较当前数据和下一个数据的大小，如果当前数据大于下一个数据的大小，就交换二者的位置，这样重复进行判断，直至排序完成返回排序后的数组。
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = -i;
+        }
+        System.out.println("排序前"+JSON.toJSONString(array));
+        System.out.println("排序后"+JSON.toJSONString(bubbleSort(array)));
+    }
+
+}
